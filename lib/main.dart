f02a6fdc0d07b81be2ef888865128e949def58bd
Ujjawal1640 github <ujjawal1696251@gmail.com>
2023-05-01 15:12:15 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,97 @@
-import 'package:firebase_core/firebase_core.dart';
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/common/theme.dart';
+import 'package:shop_app/models/cart.dart';
+import 'package:shop_app/models/catalog.dart';
+import 'package:shop_app/screens/cart.dart';
+import 'package:shop_app/screens/catalog.dart';
+import 'package:shop_app/screens/login.dart';
+
+
+void main() {
+  setupWindow();
+  runApp(const MyApp());
+}
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+void setupWindow() {
+ 
+  
+}
+
+GoRouter router() {
+  return GoRouter(
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const MyLogin(),
+      ),
+      GoRoute(
+        path: '/catalog',
+        builder: (context, state) => const MyCatalog(),
+        routes: [
+          GoRoute(
+            path: 'cart',
+            builder: (context, state) => const MyCart(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Using MultiProvider is convenient when providing multiple objects.
+    return MultiProvider(
+      providers: [
+        // In this sample app, CatalogModel never changes, so a simple Provider
+        // is sufficient.
+        Provider(create: (context) => CatalogModel()),
+        // CartModel is implemented as a ChangeNotifier, which calls for the use
+        // of ChangeNotifierProvider. Moreover, CartModel depends
+        // on CatalogModel, so a ProxyProvider is needed.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Provider Demo',
+        theme: appTheme,
+        routerConfig: router(),
+      ),
+    );
+  }
+}
+
+
+
+
+
+/*import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/config/routes.dart';
 import 'firebase_options.dart';
-import 'package:shop_app/screens/login.dart';
+import 'package:shop_app/screens/login2.dart';
 import 'package:shop_app/screens/register.dart';
 
 void main() async {
@@ -17,3 +106,4 @@ void main() async {
     routes: getRoutes(),
   ));
 }
+*/
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,103 +1,51 @@
+
+
 import 'package:flutter/material.dart';
-import 'package:shop_app/models/message.dart';
-import 'package:shop_app/models/seller.dart';
-import 'package:shop_app/screens/register.dart';
-import 'package:shop_app/services/seller_operations.dart';
-import 'package:shop_app/utils/constants.dart';
-import 'package:shop_app/utils/toast.dart';
+import 'package:go_router/go_router.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
-
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  late TextEditingController sellerCtrl;
-  late TextEditingController passwordCtrl;
-  bool isLoggedIn = false;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    sellerCtrl = TextEditingController();
-    passwordCtrl = TextEditingController();
-  }
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: SafeArea(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.network(Constants.LOGIN_IMAGE),
-          Container(
-            margin: EdgeInsets.all(16),
-            child: TextField(
-              controller: sellerCtrl,
-              decoration: InputDecoration(
-                  hintText: 'Enter your username here',
-                  prefixIcon: Icon(Icons.login),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-            ),
-          ),
-          // SizedBox(height: 10),
-          Container(
-            margin: EdgeInsets.all(16),
-            child: TextField(
-              controller: passwordCtrl,
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: 'Enter your password here',
-                  prefixIcon: Icon(Icons.password),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-            ),
-          ),
-          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                  margin: EdgeInsets.all(16),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        _doLogin().then((value) async {
-                          if (value == 1) {
-                            await Future.delayed(Duration(seconds: 1));
-                            Navigator.pushNamed(
-                                context, RoutesConstants.ADD_PRODUCT);
-                          }
-                        });
-                      },
-                      child: Text('Login'))),
-              Container(
-                  margin: EdgeInsets.all(16),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        // Navigator.push(context, MaterialPageRoute(builder: (context) => Register()));
-                        Navigator.pushNamed(context, RoutesConstants.REGISTER);
-                      },
-                      child: Text('Register')))
+              Text(
+                'Welcome',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Username',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  context.pushReplacement('/catalog');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                child: const Text('ENTER'),
+              )
             ],
-          )
-        ],
-      )),
-    ));
-  }
-
-  Future<int> _doLogin() async {
-    String sellerId = sellerCtrl.text;
-    String password = passwordCtrl.text;
-    Seller seller = Seller.takeInput(
-        userId: sellerId, password: password, appId: Constants.APP_ID);
-    SellerOperations opr = SellerOperations();
-    Message msgObject = await opr.read(seller);
-    createToast(msgObject.message, context);
-    return msgObject.code;
+          ),
+        ),
+      ),
+    );
   }
 }
